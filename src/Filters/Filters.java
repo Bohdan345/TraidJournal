2ef1117ec5a9e121c1b7c88a8fd198b1649fac8f
btishkovets@gmail.com <mwg3936Bogdan>
2022-07-14 23:10:39 +0300

    Add filters to jornal

---
+++ src/Filters/Filters.java
@@ -0,0 +1,57 @@
+package Filters;
+
+
+import Deals.DealResult;
+
+import java.util.Scanner;
+
+public class Filters {
+
+
+    Scanner scanner = new Scanner(System.in);
+    private String filteredCoinName;
+    private DealResult filteredDealResult;
+
+
+    public void coinNameFilter() {
+        System.out.println("Введите название монет для поиска : ");
+        filteredCoinName = scanner.nextLine();
+        for (int i = 0; i < Deals.Deal.getDealsList().size(); i++) {
+
+            if (Deals.Deal.getDealsList().get(i).getCoinName().equalsIgnoreCase(filteredCoinName)) {
+
+                System.out.println(Deals.Deal.getDealsList().get(i).toString(i));
+            }
+
+
+        }
+
+    }
+
+
+    public void dealResultFilter(DealResult dealResult) {
+
+
+        filteredDealResult = dealResult;
+
+
+        for (int i = 0; i < Deals.Deal.getDealsList().size(); i++) {
+
+            if (Deals.Deal.getDealsList().get(i).getDealResult() == filteredDealResult) {
+
+                System.out.println(Deals.Deal.getDealsList().get(i).toString(i));
+            }
+
+
+        }
+
+    }
+
+
+    public void dateFilter() {
+
+
+    }
+
+
+}
